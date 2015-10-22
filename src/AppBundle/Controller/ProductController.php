e7a88f0097a4f2d2a2c501446a5ac0c13c693c68
Irek <irek@lechpol.pl>
2015-10-22 12:48:38 +0200

    Dodawanie produktów do koszyka

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -8,74 +8,107 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class ProductController extends Controller
 {
-    /**
-     * @Route("/list",name="product_list")
-     */
-    public function listAction()
-    {
-        $products = $this->getProducts();
-        return $this->render('product/list.html.twig',[
-            'products' => $products
+   /**
+* @Route("/list", name="product_list")
+*/
+public function listAction()
+{
+$products = $this->getProducts();
+return $this->render('product/list.html.twig', [
+'products' => $products
+]);
+}
+/**
+* @Route("/{id}/add-to-cart", name="product_add_to_cart")
+* @Template()
+*/
+public function addToCartAction($id)
+{
+if (!$product = $this->getProduct($id)) {
+throw $this->createNotFoundException("Produkt nie znaleziony!");
+}
+// sesja
+$session = $this->get('session');
+// koszyk
+$basket = $session->get('basket', []);
+if (!array_key_exists($id, $basket)) {
+$basket[$id] = [
+'name' => $product['name'],
+'price' => $product['price'],
+'quantity' => 1
+];
+} else {
+$basket[$id]['quantity']++;
+}
+// aktualizujemy koszyk
+$session->set('basket', $basket);
+$this->addFlash('success', 'Produkt został pomyślnie dodany do koszyka.');
+return $this->redirectToRoute('product_basket');
+}
+/**
+* @Route("/basket", name="product_basket")
+*/
+public function basketAction()
+{
+$products = $this->get('session')->get('basket', []);
+return $this->render('product/basket.html.twig', [
+'products' => $products
+]);
+}
+/**
+* @Route("/{id}/remove-from-cart")
+* @Template()
+*/
+public function removeFromCartAction($id)
+{
+return array(
+// ...
+);
+}
+/**
+* @Route("/clear-basket")
+* @Template()
+*/
+public function clearBasketAction()
+{
+return array(
+// ...
+);
+}
+/**
+* Zwraca listę produktów
+*
+* @return array
+*/
+private function getProducts()
+{
+$file = file('product.txt');
+$products = [];
+foreach ($file as $p) {
+$e = explode(':', trim($p));
+$products[$e[0]] = array(
+'id' => $e[0],
+'name' => $e[1],
+'price' => $e[2],
+'description' => $e[3],
+);
+}
+return $products;
+}
 
-            ]);
-    }
-
-    /**
-     * @Route("/{id}/add-to-cart")
-     * @Template()
-     */
-    public function addToCartAction($id)
-    {
-        return array(
-                // ...
-            );    }
-
-    /**
-     * @Route("/basket")
-     * @Template()
-     */
-    public function basketAction()
-    {
-        return array(
-                // ...
-            );    }
-
-    /**
-     * @Route("/{id}/remove-from-cart")
-     * @Template()
-     */
-    public function removeFromCartAction($id)
-    {
-        return array(
-                // ...
-            );    }
-
-    /**
-     * @Route("/clear-basket")
-     * @Template()
-     */
-    public function clearBasketAction()
-    {
-        return array(
-                // ...
-            );    }
-
-     private function getProducts(){
-
-        $file = file('product.txt');
-        $products = array();
-        foreach ($file as $p){
-            $e = explode(':', trim($p));
-            $products[$e[0]] = array(
-                'id' => $e[0],
-                'name' => $e[1],
-                'price' => $e[2],
-                'description' => $e[3],
-
-            );
-        }
-        return $products;
-
-     }   
+ /**
+* Pobiera produkt o zadanym $id
+*
+* @param int $id
+* @return array
+*/
+private function getProduct($id)
+{
+$products = $this->getProducts();
+if (array_key_exists($id, $products)) {
+return $products[$id];
+}
+return null;
+}
 
 }
